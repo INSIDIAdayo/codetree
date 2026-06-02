e8b3e3a53fa1d5de1d5733c132a685ab98d1cafb
INSIDIAdayo <siho.bera@gmail.com>
2026-06-02 23:27:06 +0900

    [Passed] 변수 값 복사하기 3 (1ms, 0MB)

---
+++ trail0/변수 값 복사하기 3/copying-variable-values-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a=1;
+    int b=5;
+    int c=3;
+
+
+
+    printf("%d\n", c+c);
+    printf("%d\n", b-c);
+    printf("%d\n", c);
+
+    // Please write your code here.
+    return 0;
+}
