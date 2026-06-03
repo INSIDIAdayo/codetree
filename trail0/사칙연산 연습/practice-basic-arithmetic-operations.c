8cdf5546d96499701d914e29f955ca352280e848
INSIDIAdayo <siho.bera@gmail.com>
2026-06-03 18:03:06 +0900

    [Passed] 사칙연산 연습 (1ms, 0MB)

--- trail0/사칙연산 연습/practice-basic-arithmetic-operations.c
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int a=5;
-    int b=30;
-    int c=a+b;
+    printf("%d\n%d",4,3);
 
-    printf("%d \n",c/8);
-    printf("%d \n",c%8);
     // Please write your code here.
     return 0;
 }
