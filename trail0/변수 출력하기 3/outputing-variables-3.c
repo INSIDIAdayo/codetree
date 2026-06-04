c9bd54c1052cef6ecfc89e974abc28ca2a2a940d
INSIDIAdayo <siho.bera@gmail.com>
2026-06-04 21:31:38 +0900

    [Passed] 변수 출력하기 3 (2ms, 0MB)

---
+++ trail0/변수 출력하기 3/outputing-variables-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    char c;
+    a=1;
+    b=2;
+    c='C';
+
+    printf("%d->%d->%c\n", a, b, c);
+    // Please write your code here.
+    return 0;
+}
