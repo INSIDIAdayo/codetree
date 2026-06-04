47e3aa6c535e453ed4c77573b3f4ea9f5bbb4921
INSIDIAdayo <siho.bera@gmail.com>
2026-06-04 21:35:52 +0900

    [Passed] 세 정수형 변수 선언 (2ms, 0MB)

---
+++ trail0/세 정수형 변수 선언/declaration-of-three-natural-numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+    a = 7;
+    b = 23;
+    c = 30;
+    printf("%d + %d = %d\n", a, b, c);
+
+
+    // Please write your code here.
+    return 0;
+}
