904f643d27986cd66653d59e5d649e9323babb35
INSIDIAdayo <siho.bera@gmail.com>
2026-06-04 21:39:25 +0900

    [Passed] 달에서 무게 구하기 (1ms, 0MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    double b;
+    a=13;
+    b=0.165;
+
+    printf("%d * %lf = %lf\n",a,b,a*b);
+    // Please write your code here.
+    return 0;
+}
