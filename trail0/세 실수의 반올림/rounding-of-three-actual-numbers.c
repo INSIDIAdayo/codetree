0187290ad55413facc3d072b24a435926f09d451
INSIDIAdayo <siho.bera@gmail.com>
2026-06-06 11:51:22 +0900

    [Passed] 세 실수의 반올림 (1ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    double a,b,c;
+
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    printf("%.3lf\n", a);
+    printf("%.3lf\n", b);
+    printf("%.3lf\n", c);
+
+    // Please write your code here.
+    return 0;
+}
