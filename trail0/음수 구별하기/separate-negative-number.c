de92d9a12e4b1c6fe6eb30c306ffdb708417336b
INSIDIAdayo <siho.bera@gmail.com>
2026-06-08 18:12:17 +0900

    [Passed] 음수 구별하기 (1ms, 0MB)

---
+++ trail0/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    
+    scanf("%d",&a);
+
+    printf("%d\n",a);
+    if(a<0)
+    {
+        printf("minus");
+    }
+
+
+
+
+    // Please write your code here.
+    return 0;
+}
