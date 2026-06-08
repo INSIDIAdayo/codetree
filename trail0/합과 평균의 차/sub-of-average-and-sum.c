687742b14d8dcf63aa25a22ebe4e5e129fb1a5e0
INSIDIAdayo <siho.bera@gmail.com>
2026-06-08 16:10:03 +0900

    [Passed] 합과 평균의 차 (1ms, 0MB)

---
+++ trail0/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,b,c;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    printf("%d\n",a+b+c);
+    printf("%d\n",(a+b+c)/3);
+    printf("%d\n",(a+b+c)-((a+b+c)/3));
+
+    // Please write your code here.
+    return 0;
+}
