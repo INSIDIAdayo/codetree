3e0804a17cf891451dfc33db3b29662ce2996b54
INSIDIAdayo <siho.bera@gmail.com>
2026-06-09 09:35:12 +0900

    [Passed] 비교에 따른 연산 (2ms, 0MB)

---
+++ trail0/비교에 따른 연산/operation-based-on-comparison.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,b;
+    scanf("%d %d\n",&a,&b);
+
+    if(a>b)
+    {printf("%d",a*b);
+    }
+    else
+    {printf("%d",b/a);
+    }
+    // Please write your code here.
+    return 0;
+}
