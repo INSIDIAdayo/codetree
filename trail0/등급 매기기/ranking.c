f024369f331599743dd863153f20a06bafe0f177
INSIDIAdayo <siho.bera@gmail.com>
2026-06-11 13:07:27 +0900

    [Passed] 등급 매기기 (2ms, 0MB)

---
+++ trail0/등급 매기기/ranking.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    scanf("%d\n",&a);
+
+    if(a>=90)
+    {
+        printf("A");
+    }
+
+    else if(a>=80)
+    {
+        printf("B");
+    }
+
+    else if(a>=70)
+    {
+        printf("C");
+    }
+
+    else if(a>=60)
+    {
+        printf("D");
+    }
+
+    else
+    {
+        printf("F");
+    }
+
+    // Please write your code here.
+    return 0;
+}
