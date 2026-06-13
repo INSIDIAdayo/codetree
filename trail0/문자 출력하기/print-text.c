bb59deb49233e028d20318575f5d63f3bc21cac7
INSIDIAdayo <siho.bera@gmail.com>
2026-06-13 15:13:33 +0900

    [Passed] 문자 출력하기 (1ms, 0MB)

---
+++ trail0/문자 출력하기/print-text.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    char a;
+    int b;
+    
+    scanf("%c",&a);
+
+    for(b=0; b<8; b++)
+    {
+        printf("%c",a);
+    }
+    // Please write your code here.
+    return 0;
+}
