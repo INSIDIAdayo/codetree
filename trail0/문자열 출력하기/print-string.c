9beb77abfdccd574d4b3c0728da3e752b6328db5
INSIDIAdayo <siho.bera@gmail.com>
2026-06-13 15:16:28 +0900

    [Passed] 문자열 출력하기 (1ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,b;
+
+    scanf("%d",&a);
+
+    for(b=0; b<a; b++)
+    {
+        printf("LeebrosCode\n");
+    }
+    // Please write your code here.
+    return 0;
+}
