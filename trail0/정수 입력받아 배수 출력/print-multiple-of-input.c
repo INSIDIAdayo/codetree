096070229fb3f89a66c90b4f1280003663d6699e
INSIDIAdayo <siho.bera@gmail.com>
2026-06-15 23:11:16 +0900

    [Passed] 정수 입력받아 배수 출력 (1ms, 0MB)

---
+++ trail0/정수 입력받아 배수 출력/print-multiple-of-input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,b,c;
+
+    scanf("%d\n",&a);
+
+    for(b=1; b<6; b++)
+    {
+        c=a*b;
+        printf("%d ",c);
+    }
+    // Please write your code here.
+    return 0;
+}
