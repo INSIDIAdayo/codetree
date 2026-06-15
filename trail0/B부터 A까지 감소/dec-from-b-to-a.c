61b5d47be1d3c76da44fc76f259cc131cc05fc16
INSIDIAdayo <siho.bera@gmail.com>
2026-06-15 23:24:11 +0900

    [Passed] B부터 A까지 감소 (2ms, 0MB)

---
+++ trail0/B부터 A까지 감소/dec-from-b-to-a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,b,c;
+
+    scanf("%d %d", &a, &b);
+
+    for(c=b; c>=a; c--)
+    {
+        printf("%d ", c);
+    }
+    // Please write your code here.
+    return 0;
+}
