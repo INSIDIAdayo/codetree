5ff338a8da231add0ed5cae35960360c53d93b4f
INSIDIAdayo <siho.bera@gmail.com>
2026-08-02 21:14:10 +0900

    [Passed] 정상적인 구구단 (1ms, 0MB)

---
+++ trail0/정상적인 구구단/normal-multiple.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    // 변수 선언 및 입력
+        int n;
+            scanf("%d", &n);
+                
+                    // n*n모양 구구단을 출력합니다.
+                        for(int i = 1; i <= n; i++) {
+                                for(int j = 1; j <= n; j++) {
+                                            printf("%d * %d = %d", i, j, i * j);
+                                                        if(j != n)
+                                                                        printf(", ");
+                                                                                }
+                                                                                        printf("\n");
+                                                                                            }
+                                                                                                
+                                                                                                    return 0;
+                                                                                                    }
