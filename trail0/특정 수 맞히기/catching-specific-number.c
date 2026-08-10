fa2018a5339503c5f0db9499f43eb22e837c14c4
INSIDIAdayo <siho.bera@gmail.com>
2026-08-11 01:24:13 +0900

    [Passed] 특정 수 맞히기 (1ms, 0MB)

---
+++ trail0/특정 수 맞히기/catching-specific-number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    while(1) {
+        // 변수 선언 및 입력
+        int n;
+        scanf("%d", &n);
+        
+        // n이 25보다 작으면 Higher을, 25보다 크면 Lower을, 25와 같으면 Good을 출력한 뒤 종료합니다.
+        if(n < 25)
+            printf("Higher\n");
+        
+        else if(n > 25)
+            printf("Lower\n");
+        
+        else {
+            printf("Good\n");
+            break;
+        }
+    }
+    
+    return 0;
+}
