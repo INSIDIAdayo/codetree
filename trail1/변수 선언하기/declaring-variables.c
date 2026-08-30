aa0aafaadbee35a8175dde960d4aa6ac23a8e78b
INSIDIAdayo <siho.bera@gmail.com>
2026-08-30 20:27:08 +0900

    [Passed] 변수 선언하기 (1ms, 0MB)

---
+++ trail1/변수 선언하기/declaring-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 변수 선언
+        int a = 3;
+        	char b = 'C';
+                
+                    // 출력
+                        printf("%d\n%c\n", a, b);
+                            return 0;
+                            }
