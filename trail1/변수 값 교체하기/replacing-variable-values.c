8dd69fc9b66a27a8fd701ba1631a1d8597dbde0d
INSIDIAdayo <siho.bera@gmail.com>
2026-09-07 09:54:58 +0900

    [Passed] 변수 값 교체하기 (1ms, 0MB)

---
+++ trail1/변수 값 교체하기/replacing-variable-values.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 변수 선언
+        int a = 3;
+        	
+            	// 값 변경
+                	a = 6;
+                        
+                            // 출력
+                                printf("%d\n", a);
+                                    return 0;
+                                    }
